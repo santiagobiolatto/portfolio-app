2ff5839565ce5d4e0cd2c43d85be262ec63cac61
Santiago Biolatto <santiagobiolatto96@gmail.com>
2022-04-16 22:36:38 -0300

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,93 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { FC } from 'react';
+import './App.scss';
+import useWidth from './hooks/useWidth';
+import { Grid, Paper, Typography } from '@mui/material';
 
-function App() {
+const App: FC = () => {
+  const width = useWidth();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>Santiago Biolatto</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container className="app">
+      <Grid
+        item
+        container
+        justifyContent="center"
+        alignContent="center"
+        xs={12}
+        md={4}
+        component="header"
+        className={
+          width === 'xs' || width === 'sm'
+            ? 'sidebar-mobile'
+            : 'sidebar-desktop'
+        }>
+        <Typography variant="h3">Santiago Biolatto</Typography>
+        <Typography variant="body1">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </Typography>
+      </Grid>
+      <Grid
+        item
+        xs={12}
+        md={8}
+        component="main"
+        className={
+          width === 'xs' || width === 'sm'
+            ? 'main-content-mobile'
+            : 'main-content-desktop'
+        }>
+        <Grid container spacing={4} item xs justifyContent="center">
+          <Grid item>
+            <Paper elevation={2} className="section-container">
+              <Typography>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets
+                containing Lorem Ipsum passages, and more recently with desktop
+                publishing software like Aldus PageMaker including versions of
+                Lorem Ipsum.
+              </Typography>
+            </Paper>
+          </Grid>
+          <Grid item>
+            <Paper elevation={2} className="section-container">
+              <Typography>
+                Contrary to popular belief, Lorem Ipsum is not simply random
+                text. It has roots in a piece of classical Latin literature from
+                45 BC, making it over 2000 years old. Richard McClintock, a
+                Latin professor at Hampden-Sydney College in Virginia, looked up
+                one of the more obscure Latin words, consectetur, from a Lorem
+                Ipsum passage, and going through the cites of the word in
+                classical literature, discovered the undoubtable source. Lorem
+                Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus
+                Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero,
+                written in 45 BC. This book is a treatise on the theory of
+                ethics, very popular during the Renaissance. The first line of
+                Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line
+                in section 1.10.32. The standard chunk of Lorem Ipsum used since
+                the 1500s is reproduced below for those interested. Sections
+                1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by
+                Cicero are also reproduced in their exact original form,
+                accompanied by English versions from the 1914 translation by H.
+                Rackham.
+              </Typography>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   );
-}
+};
 
 export default App;
